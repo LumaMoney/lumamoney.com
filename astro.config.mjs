0e556b6ec46d087384672ea76c2f19eaef9827c0
Prateek Kumar <me@prateek.page>
2025-11-22 19:58:43 +0400

    github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lumamoney.com",
   vite: {
     plugins: [tailwindcss()]
   }
